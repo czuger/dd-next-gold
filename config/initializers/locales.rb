b4d2925681d99a6508042847127c9bd55270bccc
ced <zuger.cedric@gmail.com>
2017-09-01 06:42:11 +0200

    Locale working.

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,4 +1,4 @@
 I18n.config.load_path += Dir['/home/ced/ruby/dd-next-gold/config/locales/**/*.{rb,yml}']
 
-I18n.config.available_locales = [ :fr ]
+I18n.config.available_locales = [ :fr, :en ]
 I18n.default_locale = :fr
