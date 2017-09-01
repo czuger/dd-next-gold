e567d2e8d65e107b87c2eca2ef06059d8faf9e77
ced <zuger.cedric@gmail.com>
2017-09-01 06:05:58 +0200

    Routes added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'gold/weight'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  put 'set_locale/:locale', constraints: { locale: /en|fr/ }, to: 'locales#set'
+
+  root 'gold#weight'
 end
